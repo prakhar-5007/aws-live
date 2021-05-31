262e1823b5b4f2f467470cb2e781a9da8cfb398c
prakhar <prakhar.525286@gmail.com>
2021-05-31 10:55:59 +0530

    saved code

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-emp.cos479qzw0hd.us-east-2.rds.amazonaws.com"
+customuser = "prakhar"
+custompass = "Prakhar5007"
+customdb = "employee"
+custombucket = "myabs"
+customregion = "us-east-2"
